f3886dfdefcd1df5586562379d986091d2fe9d5c
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-03-31 11:53:59 +0200

    create projet client

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../Assets/argentBankLogo.png";
+
+export const Header = () => {
+  return (
+    <div>
+      {logo}
+      <div>
+        <button>sing in </button>
+      </div>
+    </div>
+  );
+};
