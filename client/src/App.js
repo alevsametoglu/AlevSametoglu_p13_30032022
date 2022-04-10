90412e3ac8cc22d5c087f6251823f95e52353a0c
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-04-10 12:45:30 +0200

    add profil page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import MainLayout from "./Layout/MainLayout";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import { Login } from "./pages/Login/Login";
+import Login from "./pages/Login/Login";
+import Profile from "./pages/profile/Profile";
 function App() {
   return (
     <Router>
@@ -9,7 +10,7 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
-
+          <Route path="profile" element={<Profile />} />
           {/* <Route path="*" element={<ErrorPage />} /> */}
         </Route>
       </Routes>
