2db3fbd3ec32fd7e8948300d74aeaf8e138f3e5a
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-04-10 12:50:14 +0200

    add navigate

--- client/src/Layout/MainLayout.jsx
+++ client/src/Layout/MainLayout.jsx
@@ -1,8 +1,20 @@
-import React from "react";
-import { Outlet } from "react-router";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router";
 import { Footer, Header } from "../components";
 
 export default function MainLayout(props) {
+  const { isAuth } = useSelector((state) => state.app);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate({ pathname: "/Profile" });
+    } else {
+      navigate({ pathname: "/" });
+    }
+  }, [isAuth]);
+
   return (
     <>
       <Header />
