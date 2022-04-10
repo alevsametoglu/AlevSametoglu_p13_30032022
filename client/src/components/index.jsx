2ef57a5afab2f4733d039dbddab79a9da6391a19
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-04-10 12:46:20 +0200

    export components

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -2,5 +2,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Banner from "./Banner/Banner";
 import Feature from "./Feature/Feature";
+import Account from "./Account/Account";
+import UpdateForm from "./UpdateForm/UpdateForm";
 
-export { Header, Footer, Banner, Feature };
+export { Header, Footer, Banner, Feature, Account, UpdateForm };
