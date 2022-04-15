22a1553b8cccb436728999ba7bd510705f1b9342
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-04-15 21:40:37 +0200

    add progress page

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,18 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Profile from "./pages/profile/Profile";
+import ProgressPage from "./pages/Progress/ProgressPage";
+import ErrorPage from "./pages/Error/ErrorPage";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="profile" element={<Profile />} />
-          {/* <Route path="*" element={<ErrorPage />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/transaction" element={<ProgressPage />} />
+          <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </Router>
